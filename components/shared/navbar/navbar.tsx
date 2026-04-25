236dd3917198fb042ee30d14758ba0fff6f255f8
HackWithNoman <mhmdnoman01@gmail.com>
2026-04-25 19:53:38 +0600

    feat: home sections

---
+++ components/shared/navbar/navbar.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { Logo } from "./logo";
+import { NavMenu } from "./nav-menu";
+import { NavigationSheet } from "./navigation-sheet";
+import { useEffect, useState } from "react";
+
+
+const Navbar = () => {
+
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <nav className="fixed inset-x-4 top-6 mx-auto h-16 max-w-(--breakpoint-xl) rounded-full border bg-background">
+      <div className="mx-auto flex h-full items-center justify-between px-4">
+        <Logo />
+
+        {/* Desktop Menu */}
+        <NavMenu className="hidden md:block" />
+
+        <div className="flex items-center gap-3">
+          <Button
+            className="hidden rounded-full sm:inline-flex"
+            variant="outline"
+          >
+            Sign In
+          </Button>
+          <Button className="rounded-full">Get Started</Button>
+
+          {/* Mobile Menu */}
+          <div className="md:hidden">
+            {mounted && <NavigationSheet />}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
